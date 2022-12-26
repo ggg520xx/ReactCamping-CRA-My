881af293d27e3761859682237204cce999a6b6c2
Kaya <ggg520xx@gmail.com>
2022-12-26 10:36:07 +0800

    Project almost 80%  api catch

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -92,6 +92,7 @@ function Layout() {
                                             <Link onClick={() => {
                                                 localStorage.clear()
                                                 setLoginStatus(false)
+                                                window.location.replace('/');
                                             }}class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">登出</Link>
                                         </li>
                                     </ul>
